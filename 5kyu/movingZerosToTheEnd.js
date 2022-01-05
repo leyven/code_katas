fa574069558d283dccc27842da33636964cb90c6
edy.cris <ecgomez@findep.com.mx>
2022-01-05 17:07:44 -0600

    kyu 5 kata moving zeros, no time for a refactor on this one

---
+++ 5kyu/movingZerosToTheEnd.js
@@ -0,0 +1,25 @@
+/**
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+*/
+
+var moveZeros = function (arr) {
+let result = [];
+let zeros = [];
+  arr.forEach((element) => {
+    if(element===0 ){
+      console.log("tig");
+      zeros.push(0);
+    }else{
+      result.push(element);
+    }
+  });
+  return result.concat(zeros);
+
+  
+}
+let x = moveZeros([9,0,9,1,2,1,1,3,1,9,0,0,9,0,0,0,0,0,0,0]);
+
+console.log( x);
